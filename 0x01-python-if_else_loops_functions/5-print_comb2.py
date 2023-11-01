4aced4b96603885952f6c0a3d793da0c1fb8e0c6
akinyeke-micheal <akinyekemicheal11@gmail.com>
2023-10-31 19:28:05 -0700

    digits2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for numbers in range(0, 100):
-    print("{}, ".format(numbers), end='')
+for numbers in range(100):
+    formatted_number = str(numbers).zfill(2)
+    print(formatted_number, end=', ' if numbers < 99 else '\n')
