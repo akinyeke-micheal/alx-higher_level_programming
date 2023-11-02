0f1d231d69f9fc88eb6c531b9ec5aa5df4576ac0
akinyeke-micheal <akinyekemicheal11@gmail.com>
2023-11-02 11:43:51 -0700

    imp4 systen and arg hidden name

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 def magic_calculation(a, b):
 
     from magic_calculation_102 import add, sub
@@ -7,7 +6,7 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return (c)
+        return c  # Removed the unnecessary parentheses
 
     else:
-        return(sub(a, b))
+        return sub(a, b)  # Use the add function for the else branchin
