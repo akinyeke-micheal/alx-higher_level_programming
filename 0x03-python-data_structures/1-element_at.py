f5b35b3b04bc9240eec8a1e57bd79cd81e9cccf3
akinyeke-micheal <akinyekemicheal11@gmail.com>
2023-11-04 07:05:44 -0700

    task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    my_list_lenght = len(my_list)
+
+    if idx < 0:
+        return none
+    elif idx > my_list_lenght:
+        return none
+    element = my_list[idx]
+    return element
+
